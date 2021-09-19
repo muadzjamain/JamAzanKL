4c454ccdffe2a18b5de3e2a919bd82483d65aba1
LeMudz <muadz.jamain@gmail.com>
2021-09-20 07:01:57 +0800

    Splash Screen FX + Logout Button
    Logout Button at User & Admin Settings page (UI)

--- Jam_azan22/app/src/main/java/com/example/jam_azan2/SplashScreen_Activity.java
+++ Jam_azan22/app/src/main/java/com/example/jam_azan2/SplashScreen_Activity.java
@@ -2,7 +2,9 @@ package com.example.jam_azan2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashScreen_Activity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class SplashScreen_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        new Handler().postDelayed(new Runnable() {
+
+            @Override public void run() {
+                Intent i = new Intent(SplashScreen_Activity.this, MainActivity.class);
+                startActivity(i);
+
+                finish();
+            }
+        }, 3000);
     }
 }
