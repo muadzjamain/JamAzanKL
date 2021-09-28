59050d5f685e2fbcbd5c24db8738e4542623d7c0
LeMudz <muadz.jamain@gmail.com>
2021-09-28 10:58:41 +0800

    Add Button to AdminSettings at AdminDashboard

--- Jam_azan22/app/src/main/java/com/example/jam_azan2/DashboardAdmin.java
+++ Jam_azan22/app/src/main/java/com/example/jam_azan2/DashboardAdmin.java
@@ -2,13 +2,32 @@ package com.example.jam_azan2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class DashboardAdmin extends AppCompatActivity {
+
+    private Button to_settings1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard_admin);
+
+        to_settings1 = (Button) findViewById(R.id.to_settings1);
+        to_settings1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openSettings();
+            }
+        });
     }
+
+    public void openSettings() {
+        Intent intent = new Intent(this, AdminSettings_Activity.class);
+        startActivity(intent);
+    }
+
 }
