e4a267240293482f00b04e103680307852f8abcf
Veenah Ganesh <88481243+Veenah07Ganesh@users.noreply.github.com>
2021-10-31 14:53:13 +0800

    payment part 1

---
+++ Jam_azan22/app/src/main/java/com/example/jam_azan2/Config/Config.java
@@ -0,0 +1,5 @@
+package com.example.jam_azan2.Config;
+
+public class Config {
+    public static final String PAYPAL_CLIENT_ID="AfMbM5qU1wgC_DOIPwJejuVzyFnZe0W6YWXZbP62Cp-lNi19gMOITHegxrzHKhmztRrCJyuJBtjkdodZ";
+}
--- Jam_azan22/app/src/main/java/com/example/jam_azan2/Derma_Activity.java
+++ Jam_azan22/app/src/main/java/com/example/jam_azan2/Derma_Activity.java
@@ -1,18 +1,76 @@
 package com.example.jam_azan2;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.example.jam_azan2.Config.Config;
+import com.paypal.android.sdk.payments.PayPalConfiguration;
+import com.paypal.android.sdk.payments.PayPalPayment;
+import com.paypal.android.sdk.payments.PayPalService;
+import com.paypal.android.sdk.payments.PaymentActivity;
+import com.paypal.android.sdk.payments.PaymentConfirmation;
+
+import org.json.JSONException;
+
+import java.math.BigDecimal;
 
 public class Derma_Activity extends AppCompatActivity {
+    private Button RM1Button;
+    private Button RM2Button;
+    private Button RM5Button;
+    private Button RM10Button;
+    private Button RM50Button;
+    private static final int PAYPAL_REQUEST_CODE = 7171;
+
+    private static PayPalConfiguration config =new PayPalConfiguration()
+            .environment(PayPalConfiguration.ENVIRONMENT_SANDBOX) //use sandbox we on test
+            .clientId(Config.PAYPAL_CLIENT_ID);
+
+    String amount="";
+
+    @Override
+    protected void onDestroy() {
+        stopService(new Intent(this, PayPalService.class));
+        super.onDestroy();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_derma);
+
+        //Start Paypal Service
+        Intent intent = new Intent(Derma_Activity.this, PayPalService.class);
+        intent.putExtra(PayPalService.EXTRA_PAYPAL_CONFIGURATION,config);
+        startService(intent);
+
+        RM1Button=findViewById(R.id.rm1button);
+        RM1Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                processPayment();
+
+            }
+
+            private void processPayment() {
+                amount="1";
+                PayPalPayment payPalPayment=new PayPalPayment(new BigDecimal(String.valueOf(amount)),"RM",
+                        "Donate for Azan",PayPalPayment.PAYMENT_INTENT_SALE);
+                Intent intent = new Intent(Derma_Activity.this, PaymentActivity.class);
+                intent.putExtra(PayPalService.EXTRA_PAYPAL_CONFIGURATION,config);
+                intent.putExtra(PaymentActivity.EXTRA_PAYMENT,payPalPayment);
+                startActivityForResult(intent,PAYPAL_REQUEST_CODE);
+
+            }
+        });
+
 
         Button to_settings1 = findViewById(R.id.to_settings1);
 
@@ -24,7 +82,32 @@ public class Derma_Activity extends AppCompatActivity {
         });
     }
 
+
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+//        //super.onActivityResult(requestCode, resultCode, data);
+//        if (requestCode == PAYPAL_REQUEST_CODE) {
+//            if (resultCode == RESULT_OK) {
+//                PaymentConfirmation confirmation = data.getParcelableExtra(PaymentActivity.EXTRA_RESULT_CONFIRMATION);
+//                if (confirmation != null) {
+//                    try {
+//                        String paymentDetails = confirmation.toJSONObject().toString(4);
+//                        startActivity(new Intent(this, PaymentActivity.class)
+//                                .putExtra("PaymentDetails", paymentDetails)
+//                                .putExtra("PaymentAmount", amount));
+//
+//                    } catch (JSONException e) {
+//                        e.printStackTrace();
+//                    }
+//                }
+//            } else if (resultCode == Activity.RESULT_CANCELED)
+//                Toast.makeText(this, "Cancel", Toast.LENGTH_SHORT).show();
+//        } else if (resultCode == PaymentActivity.RESULT_EXTRAS_INVALID)
+//            Toast.makeText(this, "Invaild", Toast.LENGTH_SHORT).show();
+//    }
+
     public void openSettings() {
         onBackPressed();
     }
 }
+
