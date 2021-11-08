8e32e3a12682968f5057bae88d1ea9a837f6b680
LeMudz <muadz.jamain@gmail.com>
2021-11-09 00:45:31 +0800

    New Background + Switch FX

--- Jam_azan22/app/src/main/java/com/example/jam_azan2/Notifikasi_Activity.java
+++ Jam_azan22/app/src/main/java/com/example/jam_azan2/Notifikasi_Activity.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 
 public class Notifikasi_Activity extends AppCompatActivity {
 
@@ -16,10 +18,29 @@ public class Notifikasi_Activity extends AppCompatActivity {
 
         Button to_settings2 = findViewById(R.id.to_settings2);
 
+        Switch switchSubuh = findViewById(R.id.subuhswitch);
+        Switch switchSyuruk = findViewById(R.id.syurukswitch);
+        Switch switchZohor = findViewById(R.id.zohorswitch);
+        Switch switchAsar = findViewById(R.id.asarswitch);
+        Switch switchMaghrib = findViewById(R.id.maghribswitch);
+        Switch switchIsyak = findViewById(R.id.isyakswitch);
+
         to_settings2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 openSettings();
+            }
+        });
+
+        switchSubuh.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+
+                }
+                else{
+
+                }
             }
         });
     }
